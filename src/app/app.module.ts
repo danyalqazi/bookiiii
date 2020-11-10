a45da8a29dfd7d18741fed88a023fefd4e3cfbb0
Danyal <danyalqazi1@yahoo.com>
2020-11-10 13:13:39 +0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -38,7 +38,11 @@ registerLocaleData(en);
     NzMenuModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule, ReactiveFormsModule,
+    
     BrowserAnimationsModule
+    
+    
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
